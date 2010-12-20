eee0d765c767fbf0ba738e5b27287c69d454812e
tejassomani <tejassomani@d402b20d-0686-0410-8cf3-3d3514b17f74>
2010-12-20 21:54:17 +0000

    Changes for the encryption/decryption of search parameters
    git-svn-id: http://www.epigenome.usc.edu/svn/ECControlCenter/trunk@61 d402b20d-0686-0410-8cf3-3d3514b17f74

--- src/edu/usc/epigenome/eccp/client/pane/flowcellReport/FlowcellReport.java
+++ src/edu/usc/epigenome/eccp/client/pane/flowcellReport/FlowcellReport.java
@@ -13,6 +13,7 @@ import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+
 import edu.usc.epigenome.eccp.client.ECService;
 import edu.usc.epigenome.eccp.client.ECServiceAsync;
 import edu.usc.epigenome.eccp.client.data.FlowcellData;
@@ -29,7 +30,9 @@ public class FlowcellReport extends ECPane
 	final TextBox laneSearchBox = new TextBox();
 	final Button searchButton = new Button("search");
 	public enum ReportType 	{ShowAll, ShowGeneus, ShowFS, ShowComplete,ShowIncomplete}
-	private ReportType reportType; 
+	private ReportType reportType;
+	String laneText = null;
+	String fcellText = null;
 	
 	public FlowcellReport(ReportType reportTypein)
 	{
@@ -53,13 +56,22 @@ public class FlowcellReport extends ECPane
 			globalSearchBox.setText(decoded);
 		}
 		if(Window.Location.getParameter("l") != null)
-		{
+		{	
 			String decoded = "";
 			for(int i=0;i<Window.Location.getParameter("l").length();i+=3)
 				decoded += (char)Integer.parseInt(Window.Location.getParameter("l").substring(i,i+3));
 			laneSearchBox.setText(decoded);
 		}
-		if(Window.Location.getParameter("g") != null || Window.Location.getParameter("l") != null)
+		if(Window.Location.getParameter("g") != null || Window.Location.getParameter("l") != null)	
+			searchPanel.setVisible(false);
+		
+		if(Window.Location.getParameter("t") != null)
+			laneText = Window.Location.getParameter("t");
+		
+		if(Window.Location.getParameter("q") != null)
+			fcellText = Window.Location.getParameter("q");
+		
+		if(Window.Location.getParameter("t") != null || Window.Location.getParameter("q") != null)
 			searchPanel.setVisible(false);
 		
 		initWidget(mainPanel);
@@ -67,17 +79,40 @@ public class FlowcellReport extends ECPane
 
 			public void onClick(ClickEvent event)
 			{
-				if(searchPanel.getWidgetCount() > 1)
-					searchPanel.remove(1);
-				String encodedGlobal = "";
-				String encodedLane = "";
-				for(char c : laneSearchBox.getText().toCharArray())
-					encodedLane += (int) c > 99 ? String.valueOf((int) c) : "0" + String.valueOf((int) c) ;
-				for(char c : globalSearchBox.getText().toCharArray())
-					encodedGlobal += (int) c > 99 ? String.valueOf((int) c) : "0" + String.valueOf((int) c) ;
-				
-				String url = "http://webapp.epigenome.usc.edu/gareports/Gareports.html?" + "r=sol&g=" + encodedGlobal + "&l=" + encodedLane;
-				searchPanel.add(new HTML("share these search results: <a href='" + url + "'>" + url + "</a>"));
+				if(searchPanel.getWidgetCount() > 1){
+					searchPanel.clear();
+					searchPanel.add(searchOptionsPanel);
+				}
+				/*
+				 * Make a RPC call to encrypt the entered text in the textboxes
+				 * on Success create the URL links for sharing the results 
+				 * and for data upload to IGV track	
+				 */
+				AsyncCallback<ArrayList<String>> encrypstring = new AsyncCallback<ArrayList<String>>(){
+
+					@Override
+					public void onFailure(Throwable caught)
+					{
+						caught.printStackTrace();
+					}
+					@Override
+					public void onSuccess(ArrayList<String> result) 
+					{
+						String url = "http://localhost:8080/gareports/Gareports.html?"+"au=sol" + "&t=" + result.get(1) + "&q=" + result.get(0);
+						searchPanel.add(new HTML("share these search results: <a href='" + url + "'>" + url  + "</a>"));
+						searchPanel.setWidth("720px");
+						//String IgvUrl = "http://localhost:8080/ECCP/Igvtrack.jsp?project=" + result.get(1) + "&fcell=" + result.get(0) + "&xml=true";
+						//searchPanel.add(new HTML("IGV track link: <a href='" + IgvUrl + "'>" + IgvUrl + "</a>"));
+						if(!(laneSearchBox.getText().isEmpty()))
+						{
+							//String IgvUrl = "http://localhost:8080/ECCP/Igvtrack.jsp?project=" + result.get(1) + "&xml=true";
+							String IgvUrl = "http://localhost:8080/ECCP/Igvtrack.jsp?project=" + result.get(1) + "&fcell=" + result.get(0) + "&";
+							searchPanel.add(new HTML("IGV Data Upload Track: <a href='" + IgvUrl + "'>" + IgvUrl + "</a>"));
+							searchPanel.setWidth("720px");
+						}
+					}
+				};
+ 	 			remoteService.getEncryptedData(globalSearchBox.getText(), laneSearchBox.getText(), encrypstring);
 				
 				vp.clear();
 				vp.add(new Image("images/progress.gif"));
@@ -135,7 +170,7 @@ public class FlowcellReport extends ECPane
 		{
 			public void onFailure(Throwable caught)
 			{
-				vp.clear();
+				vp.clear();	
 				vp.add(new Label(caught.getMessage()));
 				caught.printStackTrace();				
 			}
@@ -161,6 +196,24 @@ public class FlowcellReport extends ECPane
 			case ShowComplete: remoteService.getFlowcellsComplete(DisplayFlowcellCallback);break;
 			default: remoteService.getFlowcellsAll(DisplayFlowcellCallback);break;			
 		}		
+	}
+	
+	public void decryptKeys()
+	{
+		AsyncCallback<ArrayList<String>> GotPlainText = new AsyncCallback<ArrayList<String>>() {
+
+			@Override
+			public void onFailure(Throwable caught) {
+				caught.printStackTrace();
+			}
+
+			@Override
+			public void onSuccess(ArrayList<String> result) {	
+					globalSearchBox.setText(result.get(0));
+					laneSearchBox.setText(result.get(1));
+			}
+		};
+		remoteService.decryptKeyword(fcellText, laneText, GotPlainText);
 	}
 	
 }
