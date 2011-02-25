6692853f680b31110e901b0d333d057d724f0ffa
zack <zack@d402b20d-0686-0410-8cf3-3d3514b17f74>
2011-02-25 22:42:32 +0000

    fix max on area chart
    git-svn-id: http://www.epigenome.usc.edu/svn/ECControlCenter/trunk@89 d402b20d-0686-0410-8cf3-3d3514b17f74

--- src/edu/usc/epigenome/eccp/client/pane/flowcellReport/chart/AreaChartViewer.java
+++ src/edu/usc/epigenome/eccp/client/pane/flowcellReport/chart/AreaChartViewer.java
@@ -16,6 +16,7 @@ public class AreaChartViewer extends Composite
 		VisualizationUtils.loadVisualizationApi(new Runnable(){
 			public void run()
 			{
+						double max = 0.0;
 						String resultRows[] = inputDataCSV.split("\\n");
 						DataTable dataMatrix = DataTable.create();
 					    dataMatrix.addColumn(ColumnType.STRING, "X");
@@ -31,21 +32,23 @@ public class AreaChartViewer extends Composite
 						{									
 							String resultColumns[] = resultRows[i].split(",");
 							dataMatrix.setValue(i, 0, resultColumns[1]);
-							dataMatrix.setValue(i, 1, 100.00 * (Integer.parseInt(resultColumns[2]) / total));						
+							dataMatrix.setValue(i, 1, 100.00 * (Integer.parseInt(resultColumns[2]) / total));
+							if(100.00 * (Integer.parseInt(resultColumns[2]) / total) > max)
+							max= 100.00 * (Integer.parseInt(resultColumns[2]) / total);
 						}								
-						AreaChart motion = new AreaChart(dataMatrix, createOptions());
+						AreaChart motion = new AreaChart(dataMatrix, createOptions(max));
 						mainPanel.add(motion);
 										
 			}}, AreaChart.PACKAGE);
 		initWidget(mainPanel);
 	}
 	
-	private Options createOptions() 
+	private Options createOptions(double max) 
 	{
 	    Options options = Options.create();
 	    options.setWidth(500);
 	    options.setHeight(300);
-	    options.setMax(100.00);
+	    options.setMax(max);
 	    options.setMin(0.00);
 	    options.setTitleY("Percentage");
 	    options.setTitleX("Read");
