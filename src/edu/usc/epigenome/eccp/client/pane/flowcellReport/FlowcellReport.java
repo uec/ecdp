044ecae6b5096a45cc86401dc4090e51466747f2
tejassomani <tejassomani@d402b20d-0686-0410-8cf3-3d3514b17f74>
2011-05-02 23:45:32 +0000

    git-svn-id: http://www.epigenome.usc.edu/svn/ECControlCenter/branches/ECCPUIBinder@126 d402b20d-0686-0410-8cf3-3d3514b17f74

--- src/edu/usc/epigenome/eccp/client/pane/flowcellReport/FlowcellReport.java
+++ src/edu/usc/epigenome/eccp/client/pane/flowcellReport/FlowcellReport.java
@@ -1,218 +1,113 @@
 package edu.usc.epigenome.eccp.client.pane.flowcellReport;
+
 import java.util.ArrayList;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dev.asm.Label;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HasText;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Image;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
 
 import edu.usc.epigenome.eccp.client.ECService;
 import edu.usc.epigenome.eccp.client.ECServiceAsync;
 import edu.usc.epigenome.eccp.client.data.FlowcellData;
-import edu.usc.epigenome.eccp.client.pane.ECPane;
+import edu.usc.epigenome.eccp.client.pane.ECPaneInterface;
 
-public class FlowcellReport extends ECPane
-{
+public class FlowcellReport extends Composite implements ECPaneInterface{
+
+	private static FlowcellReportUiBinder uiBinder = GWT
+			.create(FlowcellReportUiBinder.class);
+
+	interface FlowcellReportUiBinder extends UiBinder<Widget, FlowcellReport> {
+	}
+
 	ECServiceAsync remoteService = (ECServiceAsync) GWT.create(ECService.class);
-	final VerticalPanel mainPanel = new VerticalPanel();
-	final VerticalPanel vp = new VerticalPanel();
-	final HorizontalPanel searchOptionsPanel = new HorizontalPanel();
-	final VerticalPanel searchPanel = new VerticalPanel();
-	final TextBox globalSearchBox = new TextBox();
-	final TextBox laneSearchBox = new TextBox();
-	final Button searchButton = new Button("search");
 	public enum ReportType 	{ShowAll, ShowGeneus, ShowFS, ShowComplete,ShowIncomplete}
 	private ReportType reportType;
-	String laneText = null;
-	String fcellText = null;
 	
-	public FlowcellReport(ReportType reportTypein)
-	{
-		searchPanel.add(searchOptionsPanel);
-		searchPanel.addStyleName("flowcellsearch");
-		searchOptionsPanel.add(new Label("Flowcell Properties: "));
-		searchOptionsPanel.add(globalSearchBox);
-		searchOptionsPanel.add(new Label("Lane Properties: "));
-		searchOptionsPanel.add(laneSearchBox);
-		searchOptionsPanel.add(searchButton);
-		searchOptionsPanel.addStyleName("flowcellsearch");
-		mainPanel.add(searchPanel);
-		mainPanel.add(vp);
-		reportType = reportTypein;
-		vp.add(new Image("images/progress.gif"));
-		if(Window.Location.getParameter("g") != null)
-		{
-			String decoded = "";
-			for(int i=0;i<Window.Location.getParameter("g").length();i+=3)
-				decoded += (char)Integer.parseInt(Window.Location.getParameter("g").substring(i,i+3));
-			globalSearchBox.setText(decoded);
-		}
-		if(Window.Location.getParameter("l") != null)
-		{	
-			String decoded = "";
-			for(int i=0;i<Window.Location.getParameter("l").length();i+=3)
-				decoded += (char)Integer.parseInt(Window.Location.getParameter("l").substring(i,i+3));
-			laneSearchBox.setText(decoded);
-		}
-		if(Window.Location.getParameter("g") != null || Window.Location.getParameter("l") != null)	
-			searchPanel.setVisible(false);
-		
-		if(Window.Location.getParameter("t") != null)
-			laneText = Window.Location.getParameter("t");
-		
-		if(Window.Location.getParameter("q") != null)
-			fcellText = Window.Location.getParameter("q");
-		
-		if(Window.Location.getParameter("t") != null || Window.Location.getParameter("q") != null)
-			searchPanel.setVisible(false);
-		
-		initWidget(mainPanel);
-		searchButton.addClickHandler(new ClickHandler(){
+	@UiField FlowPanel mainPanel;
+	@UiField FlowPanel searchPanel;
+	@UiField HorizontalPanel searchOptionsPanel;
+	@UiField VerticalPanel vp;
+	@UiField Button searchButton;
+	
+	public FlowcellReport(){
+		initWidget(uiBinder.createAndBindUi(this));
+		//showTool();
+	}
 
-			public void onClick(ClickEvent event)
+	public FlowcellReport(ReportType reprotTypein)
+	{
+		reportType = reprotTypein;
+		initWidget(uiBinder.createAndBindUi(this));
+		
+		searchButton.addClickHandler(new ClickHandler() 
+		{	
+			@Override
+			public void onClick(ClickEvent event) 
 			{
-				if(searchPanel.getWidgetCount() > 1){
+				if(searchPanel.getWidgetCount() > 1)
+				{
 					searchPanel.clear();
 					searchPanel.add(searchOptionsPanel);
+				}	
+			}
+		});
+		showTool();
+	}
+
+	@Override
+	public void showTool() 
+	{
+		 //Window.alert("The report type is " +reportType);
+		 
+		 AsyncCallback<ArrayList<FlowcellData>> DisplayFlowcellCallback = new AsyncCallback<ArrayList<FlowcellData>>()
+		  {
+				public void onFailure(Throwable caught)
+				{
+					vp.clear();	
+					caught.printStackTrace();				
 				}
-				/*
-				 * Make a RPC call to encrypt the entered text in the textboxes
-				 * on Success create the URL links for sharing the results 
-				 * and for data upload to IGV track	
-				 */
-				AsyncCallback<ArrayList<String>> encrypstring = new AsyncCallback<ArrayList<String>>(){
 
-					
-					public void onFailure(Throwable caught)
-					{
-						caught.printStackTrace();
-					}
-					
-					public void onSuccess(ArrayList<String> result) 
-					{
-						String url = "http://webapp.epigenome.usc.edu/gareports/Gareports.html?"+"au=sol" + "&t=" + result.get(1) + "&q=" + result.get(0);
-						searchPanel.add(new HTML("share these search results: <a href='" + url + "'>" + url  + "</a>"));
-						searchPanel.setWidth("720px");
-						//String IgvUrl = "http://localhost:8080/ECCP/Igvtrack.jsp?project=" + result.get(1) + "&fcell=" + result.get(0) + "&xml=true";
-						//searchPanel.add(new HTML("IGV track link: <a href='" + IgvUrl + "'>" + IgvUrl + "</a>"));
-						if(!(laneSearchBox.getText().isEmpty()))
-						{
-							//String IgvUrl = "http://localhost:8080/ECCP/Igvtrack.jsp?project=" + result.get(1) + "&xml=true";
-							String IgvUrl = "http://webapp.epigenome.usc.edu/ECCP/Igvtrack.jsp?project=" + result.get(1) + "&fcell=" + result.get(0) + "&";
-							searchPanel.add(new HTML("IGV Data Upload Track: <a href='" + IgvUrl + "'>" + IgvUrl + "</a>"));
-							searchPanel.setWidth("720px");
-						}
-					}
-				};
- 	 			remoteService.getEncryptedData(globalSearchBox.getText(), laneSearchBox.getText(), encrypstring);
-				
-				vp.clear();
-				vp.add(new Image("images/progress.gif"));
-				showTool();				
-			}});
-	}
-	
-	public FlowcellReport()
-	{
-		reportType = ReportType.ShowAll;
-		searchOptionsPanel.add(globalSearchBox);
-		searchOptionsPanel.add(searchButton);
-		mainPanel.add(searchOptionsPanel);
-		mainPanel.add(vp);
-		vp.add(new Image("images/progress.gif"));
-		initWidget(mainPanel);	
-		searchButton.addClickHandler(new ClickHandler(){
-
-			public void onClick(ClickEvent event)
-			{
-				vp.clear();
-				vp.add(new Image("images/progress.gif"));
-				showTool();					
-			}});
-	}
-	
-	//class FlowcellSubreport
-
-	@Override
-	public Image getToolLogo()
-	{
-		return new Image("images/report.jpg");
-	}
-
-	@Override
-	public Label getToolTitle()
-	{
-		String labelString = "All Flowcells";
-		switch(reportType)
-		{
-			case ShowGeneus: labelString="Flowcells from Geneus";break;
-			case ShowFS: labelString="Flowcells from Disk";break;
-			case ShowIncomplete: labelString="Incomplete Flowcells";break;
-			case ShowComplete: labelString="Complete Flowcells";break;
-			default: labelString="All Flowcells";break;		
-		}
-		
-		return new Label(labelString);
-	}
-
-	@Override
-	public void showTool()
-	{		
-		AsyncCallback<ArrayList<FlowcellData>> DisplayFlowcellCallback = new AsyncCallback<ArrayList<FlowcellData>>()
-		{
-			public void onFailure(Throwable caught)
-			{
-				vp.clear();	
-				vp.add(new Label(caught.getMessage()));
-				caught.printStackTrace();				
-			}
-
-			public void onSuccess(ArrayList<FlowcellData> result)
-			{
-				vp.clear();
-				for(FlowcellData flowcell : result)
-					if(flowcell.flowcellContains(globalSearchBox.getText()))
-						if(flowcell.filterLanesThatContain(laneSearchBox.getText()))
-						{
-							FlowcellSingleItem flowcellItem = new FlowcellSingleItem(flowcell);
-							vp.add(flowcellItem);
-						}
-			}
-		};	
-		
-		switch(reportType)
-		{
-			case ShowGeneus: remoteService.getFlowcellsFromGeneus(DisplayFlowcellCallback);break;
-			case ShowFS: remoteService.getFlowcellsFromFS(DisplayFlowcellCallback);break;
-			case ShowIncomplete: remoteService.getFlowcellsIncomplete(DisplayFlowcellCallback);break;
-			case ShowComplete: remoteService.getFlowcellsComplete(DisplayFlowcellCallback);break;
-			default: remoteService.getFlowcellsAll(DisplayFlowcellCallback);break;			
-		}		
-	}
-	
-	public void decryptKeys()
-	{
-		AsyncCallback<ArrayList<String>> GotPlainText = new AsyncCallback<ArrayList<String>>() {
-
+				public void onSuccess(ArrayList<FlowcellData> result)
+				{
+					vp.clear();
+					for(FlowcellData flowcell : result)
+							{
+								FlowcellSingleItem flowcellItem = new FlowcellSingleItem(flowcell);
+								vp.add(flowcellItem);
+							}
+				}
+		  };remoteService.getFlowcellsFromGeneus(DisplayFlowcellCallback);
 			
-			public void onFailure(Throwable caught) {
-				caught.printStackTrace();
-			}
-
-			public void onSuccess(ArrayList<String> result) {	
-					globalSearchBox.setText(result.get(0));
-					laneSearchBox.setText(result.get(1));
-			}
-		};
-		remoteService.decryptKeyword(fcellText, laneText, GotPlainText);
+		 //VerticalPanel toHold = new VerticalPanel();
+		//FlowcellSingleItem flowcellItem = new FlowcellSingleItem();
+		//vp.add(flowcellItem);
 	}
-	
+
+	@Override
+	public Image getToolLogo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public com.google.gwt.user.client.ui.Label getToolTitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
