6b0c16c070ecd30dcf3ad58c6008648aa58d5c0b
tejassomani <tejassomani@d402b20d-0686-0410-8cf3-3d3514b17f74>
2011-05-05 00:57:34 +0000

    code to add the composite widgets
    git-svn-id: http://www.epigenome.usc.edu/svn/ECControlCenter/branches/ECCPUIBinder@129 d402b20d-0686-0410-8cf3-3d3514b17f74

--- src/edu/usc/epigenome/eccp/client/ECCPBinderWidget.java
+++ src/edu/usc/epigenome/eccp/client/ECCPBinderWidget.java
@@ -2,14 +2,17 @@ package edu.usc.epigenome.eccp.client;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.uibinder.client.UiHandler;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.HasText;
+import com.google.gwt.user.client.ui.DecoratedTabPanel;
+import com.google.gwt.user.client.ui.FocusPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
+
+import edu.usc.epigenome.eccp.client.pane.ECPane;
+
 
 public class ECCPBinderWidget extends Composite {
 
@@ -19,13 +22,63 @@ public class ECCPBinderWidget extends Composite {
 	interface ECCPBinderWidgetUiBinder extends
 			UiBinder<Widget, ECCPBinderWidget> {
 	}
-
-	//@UiField Label ShowGeneus;
-	//@UiField HTMLPanel controlAdd;
+	
+	@UiField 
+	public static VerticalPanel addTabPanel;
+	static DecoratedTabPanel toolTabPanel = new DecoratedTabPanel();
+	
 	
 	public ECCPBinderWidget() 
 	{
 		initWidget(uiBinder.createAndBindUi(this));
 	}
 	
+	//@UiField
+	//public static TabLayoutPanel toolTabPanel;
+	/*@UiField
+	public  static HTMLPanel controlAdd;
+	
+	
+	public static  TabLayoutPanel getToolTabPanel() {
+		return toolTabPanel;
+	}
+
+	public void setToolTabPanel(TabLayoutPanel toolTabPanel) {
+		this.toolTabPanel = toolTabPanel;
+	}
+
+	public void setControlAdd(HTMLPanel controlAdd) {
+		this.controlAdd = controlAdd;
+	}
+
+	public static  HTMLPanel getControlAdd() {
+		return controlAdd;
+	}*/
+	
+	public static void addReport(final ECPane toolWidget, FocusPanel fpanel, final String typeGe)
+	{
+		fpanel.addClickHandler(new ClickHandler() {
+		
+			public void onClick(ClickEvent event) 
+			{
+				//if(toolTabPanel.)
+				
+					toolTabPanel.add(toolWidget, typeGe);
+					toolWidget.showTool();
+					
+					toolTabPanel.selectTab(toolTabPanel.getWidgetIndex(toolWidget));
+					addTabPanel.add(toolTabPanel);
+			}
+		});
+	}
+	
+	/*public static void addReport(ECPane toolWidget, String typeGe)
+	{
+		if(toolTabPanel.getWidgetIndex(toolWidget) < 0)
+		{
+			toolTabPanel.add(toolWidget, typeGe);
+			controlAdd.add(toolWidget);
+		}
+		toolTabPanel.selectTab(toolTabPanel.getWidgetIndex(toolWidget));
+	}*/
 }
