7dbbcf315dd41ab243523159fee9f049b77a87a3
tejassomani <tejassomani@d402b20d-0686-0410-8cf3-3d3514b17f74>
2011-05-17 23:31:51 +0000

    git-svn-id: http://www.epigenome.usc.edu/svn/ECControlCenter/branches/ECCPUIBinder@149 d402b20d-0686-0410-8cf3-3d3514b17f74

--- src/edu/usc/epigenome/eccp/client/pane/flowcellReport/FlowcellSingleItem.java
+++ src/edu/usc/epigenome/eccp/client/pane/flowcellReport/FlowcellSingleItem.java
@@ -1,88 +1,62 @@
 package edu.usc.epigenome.eccp.client.pane.flowcellReport;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Set;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.logical.shared.OpenEvent;
+import com.google.gwt.event.logical.shared.OpenHandler;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.DisclosurePanel;
 import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HasText;
+import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
 
 import edu.usc.epigenome.eccp.client.ECService;
 import edu.usc.epigenome.eccp.client.ECServiceAsync;
 import edu.usc.epigenome.eccp.client.data.FlowcellData;
 import edu.usc.epigenome.eccp.client.data.SampleData;
 
-public class FlowcellSingleItem extends Composite 
-{
+public class FlowcellSingleItem extends Composite  {
+
+	private static FlowcellSingleItemUiBinder uiBinder = GWT
+			.create(FlowcellSingleItemUiBinder.class);
+
+	interface FlowcellSingleItemUiBinder extends
+			UiBinder<Widget, FlowcellSingleItem> {
+	}
+
 	ECServiceAsync remoteService = (ECServiceAsync) GWT.create(ECService.class);
 	FlowcellData flowcell;
 	SampleData sampGeneus;
 	
-	public FlowcellSingleItem(final SampleData sampleIn)
-	{
-		sampGeneus = sampleIn;
-		VerticalPanel vp = new VerticalPanel();
-		for(String sampleID : sampGeneus.sampleInfo.keySet())
-		{
-			vp.add(new Label("Sample ID: " + sampleID));
-			ArrayList<FlowcellData> fcells = sampGeneus.sampleInfo.get(sampleID);
-			
-			for(int j=0;j<fcells.size();j++)
-			{
-				FlowcellData flowcell = fcells.get(j);
-				
-				FlexTable flowcellTable = new FlexTable();
-				flowcellTable.addStyleName("flowcellitem");
-				flowcellTable.setText(0,0, "Flowcell ID: " + flowcell.getFlowcellProperty("serial"));
-				flowcellTable.setText(0,1, "Lims ID: " + flowcell.getFlowcellProperty("limsID"));
-				flowcellTable.setText(0,2, flowcell.getFlowcellProperty("technician") +" " + flowcell.getFlowcellProperty("date"));
-				flowcellTable.setText(1,0, "Protocol: " + flowcell.getFlowcellProperty("protocol"));
-				flowcellTable.setText(1,1, "Status: " + flowcell.getFlowcellProperty("status"));
-				flowcellTable.setText(1,2, "Control Lane: " + flowcell.getFlowcellProperty("control"));
-				
-				FlexTable flowcellTableSample = new FlexTable();
-					
-				//HEADERS
-				flowcellTableSample.setText(0,0, "Processing");
-				flowcellTableSample.setText(0,1, "Library");
-				//flowcellTableSample.setText(0,2, "Geneusid");
-				flowcellTableSample.setText(0,2, "Organism");
-				flowcellTableSample.setText(0,3, "Project");
-				for(int i = 1; i<=flowcell.lane.keySet().size();i++)
-				{
-					flowcellTableSample.setText(i,0, flowcell.getLaneProperty(i,"processing"));
-					flowcellTableSample.setText(i,1, flowcell.getLaneProperty(i,"library"));
-					flowcellTableSample.setText(i,2, flowcell.getLaneProperty(i,"organism"));
-					
-					//String library = flowcell.getLaneProperty(i, "name").replace("+", "<br/>");
-					//flowcellTableSample.setWidget(i,1, new HTML(library));
-					//String sampleID = flowcell.getLaneProperty(i, "sampleID").replace("+", "<br/>");
-					//flowcellTableSample.setWidget(i, 2, new HTML(sampleID));
-					//String organism = flowcell.getLaneProperty(i,"organism").replace("+", "<br/>");
-					//flowcellTableSample.setWidget(i,3, new HTML(organism));
-					flowcellTableSample.setText(i,3, flowcell.getLaneProperty(i,"project"));						
-				}
-				vp.add(flowcellTable);
-				vp.add(flowcellTableSample);
-				
-			}
-		}
-		initWidget(vp);
+	public FlowcellSingleItem() {
+		initWidget(uiBinder.createAndBindUi(this));
 	}
+	@UiField FlowPanel vp;
+	@UiField FlexTable flowcellTable;
+	@UiField FlexTable flowcellTableSample;
+	@UiField DisclosurePanel qcPanel;
+	//@UiField DisclosurePanel filePanel;
+	@UiField FlowPanel qcvp;
 	
 	public FlowcellSingleItem(final FlowcellData flowcellIn)
 	{
 		flowcell=flowcellIn;
-		VerticalPanel vp = new VerticalPanel();
-		FlexTable flowcellTable = new FlexTable();
-		flowcellTable.addStyleName("flowcellitem");
+		initWidget(uiBinder.createAndBindUi(this));
+		//VerticalPanel vp = new VerticalPanel();
+		//FlexTable flowcellTable = new FlexTable();
+		//flowcellTable.addStyleName("flowcellitem");
 		flowcellTable.setText(0,0, "Flowcell ID: " + flowcell.getFlowcellProperty("serial"));
 		flowcellTable.setText(0,1, "Lims ID: " + flowcell.getFlowcellProperty("limsID"));
 		flowcellTable.setText(0,2, flowcell.getFlowcellProperty("technician") +" " + flowcell.getFlowcellProperty("date"));
@@ -90,7 +64,7 @@ public class FlowcellSingleItem extends Composite
 		flowcellTable.setText(1,1, "Status: " + flowcell.getFlowcellProperty("status"));
 		flowcellTable.setText(1,2, "Control Lane: " + flowcell.getFlowcellProperty("control"));
 		
-		FlexTable flowcellTableSample = new FlexTable();
+		//FlexTable flowcellTableSample = new FlexTable();
 			
 		//HEADERS
 		flowcellTableSample.setText(0,0, "Processing");
@@ -109,73 +83,59 @@ public class FlowcellSingleItem extends Composite
 			flowcellTableSample.setWidget(i,2, new HTML(organism));
 			flowcellTableSample.setText(i,3, flowcell.getLaneProperty(i,"project"));						
 		}
-		vp.add(flowcellTable);
-		vp.add(flowcellTableSample);
-		initWidget(vp);
-	}
-	
-	/*public FlowcellSingleItem(final SampleData flowcellIn, String sample)
-	{
-		sampGeneus=flowcellIn;
-		VerticalPanel vp = new VerticalPanel();
-	
-		for(String sampleID : flowcell.sample.keySet())
-		{
-			vp.add(new Label("Sample ID: " + sampleID));
-			
-			//ArrayList<String> fcell = flowcell.sample.get(sampleID);
-			HashMap<String,HashMap<Integer, HashMap<String, String>>> fcell = flowcell.sample.get(sampleID);
-			
-			for(String fcellID : fcell.keySet())
-			{
-				HashMap<Integer, HashMap<String, String>> laneInfo = fcell.get(fcellID);
-				FlexTable flowcellTable = new FlexTable();
-				DisclosurePanel fcPanel = new DisclosurePanel("Flowcell is " + fcellID);
-				flowcellTable.setText(0,0, "Processing");
-				flowcellTable.setText(0,1, "Library");
-				//flowcellTable.setText(0,2, "Geneusid");
-				flowcellTable.setText(0,2, "Organism");
-				flowcellTable.setText(0,3, "Project");
-				
-				int i = 1;
-				for(Integer laneNo : laneInfo.keySet())
-				{
-					HashMap<String, String> sampleInfo = laneInfo.get(laneNo);
-					flowcellTable.setText(i,0,sampleInfo.get("processing"));
-					flowcellTable.setText(i, 1, sampleInfo.get("sample_name"));
-					flowcellTable.setText(i, 2, sampleInfo.get("organism"));
-					flowcellTable.setText(i, 3, sampleInfo.get("project"));
-					i++;
-				}
-				fcPanel.add(flowcellTable);
-				vp.add(fcPanel);
-			}
-		}			
-		initWidget(vp);	
 		
-		//int noElements = fcell.size();
-		//for(int i=0;i< noElements;i++)
-		//{
-			//FlexTable flowcellTable = new FlexTable();
-			//DisclosurePanel qcPanel = new DisclosurePanel("Flowcell is ");
-			//flowcellTable.setText(i,0, "Flowcell ID: " + fcell.get(i));
-			//qcPanel.add(flowcellTable);
-			//vp.add(qcPanel);
-		//}
-		}*/
-	
-	/*public FlowcellSingleItem()
-	{
-		VerticalPanel tableHold = new VerticalPanel();
-		FlexTable flowcellTable = new FlexTable();
-		flowcellTable.setText(0,0, "Flowcell ID: " + "B03BUABXX");
-		flowcellTable.setText(0,1, "Lims ID: " + "GW2-CNX-110427-24-3043");
-		flowcellTable.setText(0,2, "Tejas" +" " + "2011-04-20");
-		flowcellTable.setText(1,0, "Protocol: " + "paired end");
-		flowcellTable.setText(1,1, "Status: " + "In geneus");
-		flowcellTable.setText(1,2, "Control Lane: " + "lane 1");
-		tableHold.add(flowcellTable);
-		initWidget(tableHold);
-	}*/
-}	
-
+		qcPanel.addOpenHandler(new OpenHandler<DisclosurePanel>()
+		{
+			public void onOpen(OpenEvent<DisclosurePanel> arg0) 
+			{
+				qcvp.add(new Image("images/progress.gif"));
+				remoteService.getQCforFlowcell(flowcell.getFlowcellProperty("serial"), new AsyncCallback<FlowcellData>()
+				{
+					public void onFailure(Throwable caught)
+					{
+						qcPanel.clear();
+						qcPanel.add(new Label(caught.getMessage()));	
+					}	
+					public void onSuccess(FlowcellData result)
+					{
+						qcvp.clear();
+						flowcell.laneQC = result.laneQC;
+						flowcell.filterLanesThatContain();
+						for(String location : flowcell.laneQC.keySet())
+						{
+							qcvp.add(new Label("QC Metrics from " + location));
+							FlexTable qcFlexTable = new FlexTable();
+							int j=0;
+							Boolean firstLine = true;
+							for(int i=1;i<=8;i++)
+							{
+								if(flowcell.laneQC.get(location).containsKey(i))
+								{	
+									j=0;
+									if(firstLine)
+									{						
+										flowcell.laneQC.get(location).get(i).remove("FlowCelln");
+										for(String s : flowcell.laneQC.get(location).get(i).keySet())
+										{								
+											qcFlexTable.setText(0, j, s);
+											j++;
+										}
+										firstLine = false;
+										j=0;
+									}									
+									
+									flowcell.laneQC.get(location).get(i).remove("FlowCelln");
+									for(String s : flowcell.laneQC.get(location).get(i).keySet())
+									{
+										qcFlexTable.setText(i, j, flowcell.laneQC.get(location).get(i).get(s));
+										j++;
+									}
+								}
+							}
+							qcvp.add(qcFlexTable);
+						}
+					}});
+			}
+		});
+	  }
+	}
