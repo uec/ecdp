1b342fabe3f18501747fb3eb435570f5becfb7c9
tejassomani <tejassomani@d402b20d-0686-0410-8cf3-3d3514b17f74>
2011-05-23 23:24:49 +0000

    git-svn-id: http://www.epigenome.usc.edu/svn/ECControlCenter/branches/ECCPUIBinder@196 d402b20d-0686-0410-8cf3-3d3514b17f74

--- src/edu/usc/epigenome/eccp/client/pane/sampleReport/SampleSingleReport.java
+++ src/edu/usc/epigenome/eccp/client/pane/sampleReport/SampleSingleReport.java
@@ -2,6 +2,8 @@ package edu.usc.epigenome.eccp.client.pane.sampleReport;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.logical.shared.CloseEvent;
+import com.google.gwt.event.logical.shared.CloseHandler;
 import com.google.gwt.event.logical.shared.OpenEvent;
 import com.google.gwt.event.logical.shared.OpenHandler;
 import com.google.gwt.uibinder.client.UiBinder;
@@ -10,6 +12,7 @@ import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.DisclosurePanel;
 import com.google.gwt.user.client.ui.DisclosurePanelImages;
@@ -18,12 +21,16 @@ import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HasText;
 import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Tree;
+import com.google.gwt.user.client.ui.TreeItem;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 import edu.usc.epigenome.eccp.client.ECService;
 import edu.usc.epigenome.eccp.client.ECServiceAsync;
+import edu.usc.epigenome.eccp.client.Resources.UserPanelResources;
 import edu.usc.epigenome.eccp.client.data.SampleData;
 
 public class SampleSingleReport extends Composite {
@@ -34,6 +41,10 @@ public class SampleSingleReport extends Composite {
 	interface SampleSingleReportUiBinder extends
 			UiBinder<Widget, SampleSingleReport> {
 	}
+	
+	static {
+	    UserPanelResources.INSTANCE.userPanel().ensureInjected();  
+	}
 
 	ECServiceAsync remoteService = (ECServiceAsync) GWT.create(ECService.class);
 	SampleData sampGeneus;
@@ -41,46 +52,89 @@ public class SampleSingleReport extends Composite {
 	public SampleSingleReport() {
 		initWidget(uiBinder.createAndBindUi(this));
 	}
-
-	@UiField FlowPanel vp;
-	@UiField FlexTable sampleData;
-	@UiField FlowPanel dataDisplay;
-	//@UiField FlexTable flowcellInfo;
-	@UiField DisclosurePanel sampleInfoPanel;
+	
+	/*//@UiField TreeItem sampleData;
+	@UiField Tree root;
+	//@UiField FlexTable data;
 	
 	public SampleSingleReport(final SampleData sampleIn)
 	{
 		sampGeneus = sampleIn;
 		initWidget(uiBinder.createAndBindUi(this));
 		
+		StringBuffer sb = new StringBuffer("Library: " + sampGeneus.getSampleProperty("library"));
+		sb.append("    Project: " + sampGeneus.getSampleProperty("project"));
+		//sampleData.setHTML(sb.toString());
+		//FlexTable data = new FlexTable();
+		TreeItem sampleData = new TreeItem("Library");
+		root.addItem(sampleData);
+		//sampleData.addItem(data);
+		//data.setText(0, 0, "Library: " + sampGeneus.getSampleProperty("library"));
+		//data.setText(0, 1, "Project: " + sampGeneus.getSampleProperty("project"));
+		//data.setText(0, 2, "Organism: " + sampGeneus.getSampleProperty("organism"));
+		//data.setText(0, 3, "Date: " + sampGeneus.getSampleProperty("date"));
+		
+	}*/
+
+	@UiField FlowPanel vp;
+	@UiField FlexTable sampleData;
+	@UiField FlowPanel dataDisplay;
+	@UiField DisclosurePanel sampleInfoPanel;
+	@UiField HorizontalPanel sampHeader;
+	@UiField Image headerIcon;
+	
+	public SampleSingleReport(final SampleData sampleIn)
+	{
+		sampGeneus = sampleIn;
+		initWidget(uiBinder.createAndBindUi(this));
+		
+		//headerIcon.setUrl("images/rightArrow.png");
 		sampleData.setText(0, 0, "Library: " + sampGeneus.getSampleProperty("library"));
 		sampleData.setText(0, 1, "Project: " + sampGeneus.getSampleProperty("project"));
 		sampleData.setText(0, 2, "Organism: " + sampGeneus.getSampleProperty("organism"));
 		sampleData.setText(0, 3, "Date: " + sampGeneus.getSampleProperty("date"));
 		
+		
+		sampleInfoPanel.addCloseHandler(new CloseHandler<DisclosurePanel>() 
+		{	
+			public void onClose(CloseEvent<DisclosurePanel> arg0) 
+			{
+				headerIcon.setUrl("images/rightArrow.png");
+			}
+		});
 		sampleInfoPanel.addOpenHandler(new OpenHandler<DisclosurePanel>() 
 		{
 			public void onOpen(OpenEvent<DisclosurePanel> arg0) 
 			{
+				headerIcon.setUrl("images/downArrow.png");
 				dataDisplay.clear();
 				for(final String flowcellSerial : sampGeneus.flowcellInfo.keySet())
 				{
-					Label fcellInfo = new Label();
-					fcellInfo.setText("Flowcell ID: " + flowcellSerial + "\t Technician: " + sampGeneus.flowcellInfo.get(flowcellSerial).get("technician"));
-					//FlexTable fcellInfo  = new FlexTable();
-					//fcellInfo.setText(0,0,"Flowcell ID: " + flowcellSerial);
-					//fcellInfo.setText(0, 1, "Technician: " + sampGeneus.flowcellInfo.get(flowcellSerial).get("technician"));
+					HorizontalPanel fcellPanel = new HorizontalPanel();
+					FlexTable fcellInfo  = new FlexTable();
+					final Image fcellInfoImage = new Image("images/rightArrow.png");
+					fcellInfo.setText(0,0,"Flowcell ID: " + flowcellSerial);
+					fcellInfo.setText(0, 1, "Technician: " + sampGeneus.flowcellInfo.get(flowcellSerial).get("technician"));
+					
+					fcellPanel.add(fcellInfoImage);
+					fcellPanel.add(fcellInfo);
+					
 					@SuppressWarnings("deprecation")
-					final DisclosurePanel flowcellShow = new DisclosurePanel(fcellInfo);
+					final DisclosurePanel flowcellShow = new DisclosurePanel(fcellPanel);
 					flowcellShow.setAnimationEnabled(true);
-					//flowcellShow.setHeader(fcellInfo);
-					//flowcellShow.
-					//final DisclosurePanel flowcellShow = new DisclosurePanel
+					
+					flowcellShow.addCloseHandler(new CloseHandler<DisclosurePanel>() {
+
+						public void onClose(CloseEvent<DisclosurePanel> arg0) {
+								
+							fcellInfoImage.setUrl("images/rightArrow.png");
+						}});
 					
 					flowcellShow.addOpenHandler(new OpenHandler<DisclosurePanel>() 
 					{
 						public void onOpen(OpenEvent<DisclosurePanel> arg0) 
 						{
+							fcellInfoImage.setUrl("images/downArrow.png");
 							remoteService.getLaneFlowcellSample(sampGeneus.sampleProperties.get("library"), flowcellSerial, new AsyncCallback<SampleData>() 
 							{
 								public void onFailure(Throwable arg0) 
@@ -91,25 +145,38 @@ public class SampleSingleReport extends Composite {
 								{
 									flowcellShow.clear();
 									sampGeneus.flowcellLane = result.flowcellLane;
-									VerticalPanel holdLane = new VerticalPanel();
+									FlowPanel holdLane = new FlowPanel();
 									flowcellShow.add(holdLane);
 									
 									for(final Integer laneNo : sampGeneus.flowcellLane.keySet())
 									{
+										HorizontalPanel holdLanePanel = new HorizontalPanel();
+										//content of flowcellshow panel is holdLane (consisting of lanes and images)
+										
 										FlexTable laneInfoTable = new FlexTable();
+										final Image laneInfoImage = new Image("images/rightArrow.png");
 										laneInfoTable.setText(0,0,"Lane No: " + laneNo);
 										laneInfoTable.setText(0,1,"Processing: " + sampGeneus.flowcellLane.get(laneNo).get("processing"));
+										holdLanePanel.add(laneInfoImage);
+										holdLanePanel.add(laneInfoTable);
 										
 										@SuppressWarnings("deprecation")
-										final DisclosurePanel laneInfoPanel = new DisclosurePanel(laneInfoTable);
+										final DisclosurePanel laneInfoPanel = new DisclosurePanel(holdLanePanel);
 										laneInfoPanel.setAnimationEnabled(true);
 										//laneInfoPanel.setHeader(laneInfoTable);
+										
+										laneInfoPanel.addCloseHandler(new CloseHandler<DisclosurePanel>(){
+											public void onClose(CloseEvent<DisclosurePanel> arg0) {
+												laneInfoImage.setUrl("images/downArrow.png");
+											}});
 										
 										laneInfoPanel.addOpenHandler(new OpenHandler<DisclosurePanel>()
 										{
 											public void onOpen(OpenEvent<DisclosurePanel> arg0) 
 											{
+												laneInfoImage.setUrl("images/downArrow.png");
 												laneInfoPanel.clear();
+												
 												FlowPanel ReportPanel = new FlowPanel();
 												FlowPanel DownloadPanel = new FlowPanel();
 												FlowPanel QCDownloadPanel = new FlowPanel();
