d9f75b7578ebbf9c341171635aaff28aa4d68cea
tejassomani <tejassomani@d402b20d-0686-0410-8cf3-3d3514b17f74>
2011-07-29 22:20:46 +0000

    git-svn-id: http://www.epigenome.usc.edu/svn/ECControlCenter/branches/ECCPUIBinder@243 d402b20d-0686-0410-8cf3-3d3514b17f74

--- src/edu/usc/epigenome/eccp/client/pane/flowcellReport/chart/ChartViewer.java
+++ src/edu/usc/epigenome/eccp/client/pane/flowcellReport/chart/ChartViewer.java
@@ -1,23 +1,42 @@
 package edu.usc.epigenome.eccp.client.pane.flowcellReport.chart;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.DecoratedPopupPanel;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HasText;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+
 import edu.usc.epigenome.eccp.client.ECService;
 import edu.usc.epigenome.eccp.client.ECServiceAsync;
+import edu.usc.epigenome.eccp.client.Resources.UserPanelResources;
 
-public class ChartViewer extends Composite
+public class ChartViewer extends Composite  
 {
-	ECServiceAsync remoteService = (ECServiceAsync) GWT.create(ECService.class);	
-	VerticalPanel mainPanel = new VerticalPanel();
-	VerticalPanel chartPanel = new VerticalPanel();
-	DecoratedPopupPanel popup = new DecoratedPopupPanel();
-	Button closeButton = new Button("close");
+	private static ChartViewerUiBinder uiBinder = GWT
+			.create(ChartViewerUiBinder.class);
+
+	interface ChartViewerUiBinder extends UiBinder<Widget, ChartViewer> {
+	}
+	static {
+	    UserPanelResources.INSTANCE.userPanel().ensureInjected();  
+	}
+	ECServiceAsync remoteService = (ECServiceAsync) GWT.create(ECService.class);
+	
+	@UiField Label view;
+	@UiField FlowPanel mainPanel;
+	@UiField FlowPanel chartPanel;
+	@UiField FlowPanel popup;
+	@UiField Button closeButton;
+
 	public static enum ChartType
 	{
 		Area,
@@ -25,38 +44,38 @@ public class ChartViewer extends Composite
 		Column
 	}
 	
+	public ChartViewer() {
+		initWidget(uiBinder.createAndBindUi(this));
+	}
+	
 	
 	public ChartViewer(final String csvPath, final ChartType t)
 	{
-		final Label view = new Label(": view chart");
-		view.addStyleName("viewchartlabel");
+		initWidget(uiBinder.createAndBindUi(this));
 		
-		mainPanel.add(new Label(csvPath.replaceAll(".+/", "")));
-		mainPanel.add(chartPanel);
-		mainPanel.add(closeButton);
+		closeButton.setVisible(false);
 		
-	    popup.add(mainPanel);
-	    closeButton.addClickHandler(new ClickHandler(){
-			public void onClick(ClickEvent event)
+		closeButton.addClickHandler(new ClickHandler()
+		{
+			public void onClick(ClickEvent arg0) 
 			{
-				popup.hide();				
-			}});
-	    
-	    view.addClickHandler(new ClickHandler()
-	    {
-			public void onClick(ClickEvent event)
-			{
-				popup.showRelativeTo(view);
 				chartPanel.clear();
-				chartPanel.add(new Label ("Loading data"));
-				remoteService.getCSVFromDisk(csvPath, new AsyncCallback<String>()
-				{
-					public void onFailure(Throwable caught)
-					{
-						chartPanel.clear();
-						chartPanel.add(new Label ("Error loading data!"));
-					}
-					public void onSuccess(String result)
+				closeButton.setVisible(false);
+			}
+		});
+	
+		//showChart(t, csvPath);
+		
+		view.addClickHandler(new ClickHandler()
+		{
+			public void onClick(ClickEvent arg0) 
+			{
+				//chartPanel.clear();
+				//chartPanel.add(new Label ("Loading data"));
+				
+				remoteService.getCSVFromDisk(csvPath, new AsyncCallback<String>() 
+				{	
+					public void onSuccess(String result) 
 					{
 						chartPanel.clear();
 						if(t == ChartType.ResultCount)
@@ -65,10 +84,42 @@ public class ChartViewer extends Composite
 							chartPanel.add(new AreaChartViewer(result));
 						else if(t == ChartType.Column)
 							chartPanel.add(new ColumnChartViewer(result));
+						
+						closeButton.setVisible(true);
 					}
-				});				
-			}
-		});
-		initWidget(view);	
+					public void onFailure(Throwable arg0) 
+					{
+						chartPanel.clear();
+						chartPanel.add(new Label ("Error loading data!"));	
+					}
+				});
+			}});
+		
 	}
+	
+	public void showChart(final ChartType t, final String csvPath)
+	{
+		AsyncCallback<String> CSVCallback = new AsyncCallback<String>()
+		{	
+			@Override
+			public void onSuccess(String result) 
+			{
+				//chartPanel.clear();
+				if(t == ChartType.ResultCount)
+					chartPanel.add(new MotionChartViewer(result));
+				else if(t == ChartType.Area)
+					chartPanel.add(new AreaChartViewer(result));
+				else if(t == ChartType.Column)
+					chartPanel.add(new ColumnChartViewer(result));
+			}
+			
+			@Override
+			public void onFailure(Throwable arg0) 
+			{
+				//chartPanel.clear();
+				chartPanel.add(new Label ("Error loading data!"));	
+			}
+		};remoteService.getCSVFromDisk(csvPath, CSVCallback);
+	}
+
 }
