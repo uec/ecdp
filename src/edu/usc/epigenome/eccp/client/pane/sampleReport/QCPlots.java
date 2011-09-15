2333d626c65a7fbcc328678757e0f3b8223a1ae2
tejassomani <tejassomani@d402b20d-0686-0410-8cf3-3d3514b17f74>
2011-09-15 18:58:57 +0000

    git-svn-id: http://www.epigenome.usc.edu/svn/ECControlCenter/branches/ECCPUIBinder@284 d402b20d-0686-0410-8cf3-3d3514b17f74

--- src/edu/usc/epigenome/eccp/client/pane/sampleReport/QCPlots.java
+++ src/edu/usc/epigenome/eccp/client/pane/sampleReport/QCPlots.java
@@ -1,5 +1,6 @@
 package edu.usc.epigenome.eccp.client.pane.sampleReport;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedHashMap;
@@ -9,26 +10,23 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 import edu.usc.epigenome.eccp.client.ECCPBinderWidget;
+import edu.usc.epigenome.eccp.client.ECControlCenter;
 import edu.usc.epigenome.eccp.client.ECService;
 import edu.usc.epigenome.eccp.client.ECServiceAsync;
+import edu.usc.epigenome.eccp.client.GenUserBinderWidget;
 import edu.usc.epigenome.eccp.client.Resources.UserPanelResources;
+import edu.usc.epigenome.eccp.client.data.FlowcellData;
 import edu.usc.epigenome.eccp.client.data.SampleData;
-import edu.usc.epigenome.eccp.client.pane.flowcellReport.chart.AreaChartViewer;
-import edu.usc.epigenome.eccp.client.pane.flowcellReport.chart.ChartBrowser;
 import edu.usc.epigenome.eccp.client.pane.flowcellReport.chart.ChartViewer;
-import edu.usc.epigenome.eccp.client.pane.flowcellReport.chart.ColumnChartViewer;
-import edu.usc.epigenome.eccp.client.pane.flowcellReport.chart.MotionChartViewer;
 import edu.usc.epigenome.eccp.client.pane.flowcellReport.chart.ChartViewer.ChartType;
 
 public class QCPlots extends Composite {
@@ -44,22 +42,25 @@ public class QCPlots extends Composite {
 	ECServiceAsync remoteService = (ECServiceAsync) GWT.create(ECService.class);
 	String flowcellSerial;
 	int laneNo;
+	String run;
 	SampleData sample;
 	
 	@UiField Label showPlots;
-	@UiField FlowPanel popup;
+	@UiField VerticalPanel popup;
 	@UiField FlowPanel summaryChart;
 	@UiField FlowPanel mainPanel;
+	@UiField Label path;
 	
 	public QCPlots() {
 		initWidget(uiBinder.createAndBindUi(this));
 	}
 	
-	public QCPlots(final SampleData sampleIn, final String flowcellSerialIn, final int lane)
+	public QCPlots(final SampleData sampleIn, final String flowcellSerialIn, final int lane, final String runId)
 	{
 		flowcellSerial = flowcellSerialIn;
 		sample = sampleIn;
 		laneNo = lane;
+		run = runId;
 		initWidget(uiBinder.createAndBindUi(this));
 		
 		showPlots.addClickHandler(new ClickHandler() {
@@ -67,34 +68,65 @@ public class QCPlots extends Composite {
 			public void onClick(ClickEvent arg0) 
 			{
 				
-				ECCPBinderWidget.addtoTab(popup, "Plots  " + sample.getSampleProperty("library") + " " + flowcellSerial + " " + lane);
-				//summaryChart.clear();
-				//summaryChart.add(new Image("images/progress.gif"));
+				if(ECControlCenter.getUserType().equalsIgnoreCase("super"))
+					ECCPBinderWidget.addtoTab(popup, "Plots" + sample.getSampleProperty("library") + "_" + flowcellSerial);
+				else if(ECControlCenter.getUserType().equalsIgnoreCase("guest"))
+					GenUserBinderWidget.addtoTab(popup, "Plots" + sample.getSampleProperty("library") + "_" + flowcellSerial);
 				
-				remoteService.getCSVFiles(flowcellSerial, new AsyncCallback<SampleData>() 
+				remoteService.getCSVFiles(run, flowcellSerial, sample.getSampleProperty("geneusID_sample"), new AsyncCallback<FlowcellData>() 
 				{	
 					public void onFailure(Throwable caught) 
 					{
 						summaryChart.clear();
-						summaryChart.add(new Label(caught.getMessage()));
+						path.setText(caught.getMessage());
+						//summaryChart.add(new Label(caught.getMessage()));
 					}
-					public void onSuccess(SampleData result) 
+					public void onSuccess(FlowcellData result) 
 					{
 						summaryChart.clear();
-						sample.flowcellFileList = result.flowcellFileList;
-						sample.filterFiles(lane);
-						//sortBy("base");
-						ChartBrowser chBrowse = new ChartBrowser(sample.flowcellFileList);
-						summaryChart.add(chBrowse);
-						/*for(int n=0; n<sample.flowcellFileList.size(); n++)
+						//path.setText("Sample:" + sample.getSampleProperty("library") + " > Flowcell:" + flowcellSerial + " > Lane:"+ laneNo + " > Run:" + run);
+						summaryChart.add(new Label("Sample:" + sample.getSampleProperty("library") + " > Flowcell:" + flowcellSerial + " > Lane:"+ laneNo + " > Run:" + run));
+						sample.sampleFlowcells.get(flowcellSerial).fileList = result.fileList;
+						sample.sampleFlowcells.get(flowcellSerial).filterFiles(lane, sample.getSampleProperty("geneusID_sample"), run);
+						sample.sampleFlowcells.get(flowcellSerial).fileList = sortBy("base");
+						//ChartBrowser chBrowse = new ChartBrowser(sample.sampleFlowcells.get(flowcellSerial).fileList);
+						//summaryChart.add(chBrowse);
+						for(int n=0; n<sample.sampleFlowcells.get(flowcellSerial).fileList.size(); n++)
 						{
-							LinkedHashMap<String, String> f = sample.flowcellFileList.get(n);
-							String fileURI = f.containsKey("encfullpath") ? "http://webapp.epigenome.usc.edu/ECCP/retrieve.jsp?resource=" + f.get("encfullpath") :  "http://www.epigenome.usc.edu/webmounts/" + f.get("dir") + "/" + f.get("base");
-							summaryChart.add(new HTML("<a target=\"new\" href=\"" + fileURI + "\">" + f.get("base") + "</a>"));
-						}*/
+							LinkedHashMap<String, String> f = sample.sampleFlowcells.get(flowcellSerial).fileList.get(n);
+							String fileURI = f.containsKey("encfullpath") ? "http://webapp.epigenome.usc.edu/ECCPBinder/retrieve.jsp?resource=" + f.get("encfullpath") :  "http://www.epigenome.usc.edu/webmounts/" + f.get("dir") + "/" + f.get("base");
+							if(f.get("base").contains("ReadCount") && f.get("base").contains(".csv"))
+							{
+								summaryChart.add(new HTML("<a target=\"new\" href=\"" + fileURI + "\">" + f.get("base") + "</a>"));
+								summaryChart.add(new ChartViewer(f.get("fullpath"), ChartType.Area));
+							}
+							else if(f.get("base").contains("nmerCount") && f.get("base").contains(".csv"))
+							{
+								summaryChart.add(new HTML("<a target=\"new\" href=\"" + fileURI + "\">" + f.get("base") + "</a>"));
+								summaryChart.add(new ChartViewer(f.get("fullpath"), ChartType.Column));
+							}
+							else if(f.get("base").contains("ResultCount") && f.get("base").contains(".csv"))
+							{
+								summaryChart.add(new HTML("<a target=\"new\" href=\"" + fileURI + "\">" + f.get("base") + "</a>"));
+								summaryChart.add(new ChartViewer(f.get("fullpath"), ChartType.ResultCount));
+							}
+						}
 					}	
 			});
 		}});
+	}
+	
+	public ArrayList<LinkedHashMap<String,String>> sortBy(final String key)
+	{
+		final ArrayList<LinkedHashMap<String,String>> sortedFiles = (ArrayList<LinkedHashMap<String, String>>) sample.sampleFlowcells.get(flowcellSerial).fileList.clone();
+		Collections.sort(sortedFiles, new Comparator<LinkedHashMap<String, String>>()
+		{
+			public int compare(LinkedHashMap<String, String> o1, LinkedHashMap<String, String> o2)
+			{
+				return o1.get(key).compareTo(o2.get(key));
+			}
+		});
+		return sortedFiles;
 	}
 	
 /*	public void showChart(final String csvPath,final ChartType t, final String fileURI, final String base)
