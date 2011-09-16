ca70494ade31a2878c9a029f077f0f90ad172a28
tejassomani <tejassomani@d402b20d-0686-0410-8cf3-3d3514b17f74>
2011-09-16 00:25:59 +0000

    git-svn-id: http://www.epigenome.usc.edu/svn/ECControlCenter/branches/ECCPUIBinder@295 d402b20d-0686-0410-8cf3-3d3514b17f74

--- src/edu/usc/epigenome/eccp/client/ECServiceAsync.java
+++ src/edu/usc/epigenome/eccp/client/ECServiceAsync.java
@@ -18,15 +18,17 @@ public interface ECServiceAsync
 	void getFlowcellsIncomplete(AsyncCallback<ArrayList<FlowcellData>> callback) throws IllegalArgumentException;
 	void getFlowcellsComplete(AsyncCallback<ArrayList<FlowcellData>> callback) throws IllegalArgumentException;
 	void getSampleFromGeneus(AsyncCallback<ArrayList<SampleData>> callback) throws IllegalArgumentException;
+	//Get QC for given flowcell (flowcell view)
 	void getQCforFlowcell(String serial,AsyncCallback<FlowcellData> callback) throws IllegalArgumentException;
-	void getQCSampleFlowcell(String serial, String sampleID,
-			AsyncCallback<FlowcellData> callback);
+	//Get QC for the given flowcell, sample and lane (sample view)
+	void getQCSampleFlowcell(String serial, String sampleID,int laneNo, AsyncCallback<FlowcellData> callback);
 	
+	//Get files for a given flowcell (flowcell view)
 	void getFilesforFlowcell(String serial,AsyncCallback<FlowcellData> callback) throws IllegalArgumentException;
-	
-	//void getFilesforFlowcellLane(String serial, AsyncCallback<SampleData> callback) throws IllegalArgumentException;
-	void getCSVFiles(String serial, AsyncCallback<FlowcellData> callback) throws IllegalArgumentException;
-	
+	//Get the files for Files Download section for the sample view
+	void getFilesforRunSample(String run_id, String serial, String sampleID, AsyncCallback<FlowcellData> callback) throws IllegalArgumentException;
+	//Get the files for the plots
+	void getCSVFiles(String run_id, String serial, String sampleID, AsyncCallback<FlowcellData> callback) throws IllegalArgumentException;
 	void getCSVFromDisk(String filePath, AsyncCallback<String> String) throws IllegalArgumentException;
 	void getAnalysisFromFS(AsyncCallback<ArrayList<FlowcellData>> callback) throws IllegalArgumentException;
 	
