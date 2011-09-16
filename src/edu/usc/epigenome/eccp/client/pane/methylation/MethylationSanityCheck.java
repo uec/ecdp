436f88494a78b94de307b9eb9dbb671af7671955
tejassomani <tejassomani@d402b20d-0686-0410-8cf3-3d3514b17f74>
2011-09-16 21:38:58 +0000

    git-svn-id: http://www.epigenome.usc.edu/svn/ECControlCenter/branches/ECCPUIBinder@302 d402b20d-0686-0410-8cf3-3d3514b17f74

--- src/edu/usc/epigenome/eccp/client/pane/methylation/MethylationSanityCheck.java
+++ src/edu/usc/epigenome/eccp/client/pane/methylation/MethylationSanityCheck.java
@@ -1,69 +1,89 @@
 package edu.usc.epigenome.eccp.client.pane.methylation;
+
 import java.util.ArrayList;
 import java.util.Collections;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HasText;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.TextArea;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
 
 import edu.usc.epigenome.eccp.client.ECService;
 import edu.usc.epigenome.eccp.client.ECServiceAsync;
+import edu.usc.epigenome.eccp.client.Resources.UserPanelResources;
 import edu.usc.epigenome.eccp.client.data.MethylationData;
 import edu.usc.epigenome.eccp.client.pane.ECPane;
 
-public class MethylationSanityCheck extends ECPane
-{
-		ECServiceAsync remoteService = (ECServiceAsync) GWT.create(ECService.class);
-		final VerticalPanel vp = new VerticalPanel();
-		public MethylationSanityCheck()
-		{
-			initWidget(vp);	
-		}
-		@Override
-		public Image getToolLogo()
-		{	
-			return new Image("images/globe_process.png");
-		}
+public class MethylationSanityCheck extends ECPane{
 
-		@Override
-		public Label getToolTitle()
-		{
-			return new Label("Methylation Sanity Check");
-		}
+	private static MethylationSanityCheckUiBinder uiBinder = GWT
+			.create(MethylationSanityCheckUiBinder.class);
 
-		@Override
-		public void showTool()
+	interface MethylationSanityCheckUiBinder extends
+			UiBinder<Widget, MethylationSanityCheck> {
+	}
+
+	static {
+	    UserPanelResources.INSTANCE.userPanel().ensureInjected();  
+	}
+
+	ECServiceAsync remoteService = (ECServiceAsync) GWT.create(ECService.class);
+	
+	@UiField FlowPanel vp;
+	@UiField TextArea ta;
+	
+	public MethylationSanityCheck() {
+		initWidget(uiBinder.createAndBindUi(this));
+	}
+
+	
+	public void showTool()
+	{
+		remoteService.getMethFromGeneus(new AsyncCallback<ArrayList<MethylationData>>()
 		{
-			final TextArea ta = new TextArea();
-			ta.setWidth("800px");
-			ta.setHeight("600px");
-			remoteService.getMethFromGeneus(new AsyncCallback<ArrayList<MethylationData>>()
+			public void onFailure(Throwable caught)
 			{
-				public void onFailure(Throwable caught)
-				{
-					// TODO Auto-generated method stub
-					
-				}
+				caught.getMessage();
+			}
 
-				public void onSuccess(ArrayList<MethylationData> result)
+			public void onSuccess(ArrayList<MethylationData> result)
+			{
+				String errorString = "";
+				ArrayList<String> errors = result.get(0).validateIntegrity(result);
+				Collections.sort(errors);
+				for(String e : errors)
 				{
-					String errorString = "";
-					ArrayList<String> errors = result.get(0).validateIntegrity(result);
-					Collections.sort(errors);
-					for(String e : errors)
-					{
-						errorString += e + "\n";
-					}
-					ta.setText(errorString);
-					
+					errorString += e + "\n";
 				}
-			});
-			vp.add(ta);		
-		}
+				ta.setText(errorString);
+				
+			}
+		});
+	}
+
+
+	@Override
+	public Image getToolLogo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Label getToolTitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 
 }
-
