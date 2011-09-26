07347258ffb156ca3bb5c822f4adc28b24fb2acc
tejassomani <tejassomani@d402b20d-0686-0410-8cf3-3d3514b17f74>
2011-09-26 22:14:30 +0000

    git-svn-id: http://www.epigenome.usc.edu/svn/ECControlCenter/branches/ECCPUIBinder@324 d402b20d-0686-0410-8cf3-3d3514b17f74

--- src/edu/usc/epigenome/eccp/client/ECService.java
+++ src/edu/usc/epigenome/eccp/client/ECService.java
@@ -2,6 +2,7 @@ package edu.usc.epigenome.eccp.client;
 
 import java.util.ArrayList;
 
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 import edu.usc.epigenome.eccp.client.data.FlowcellData;
@@ -14,27 +15,43 @@ import edu.usc.epigenome.eccp.client.data.SampleData;
 @RemoteServiceRelativePath("ecservice")
 public interface ECService extends RemoteService
 {
-	//Flowcell reporting
+	/*
+	 * Flowcell reporting
+	 */
 	ArrayList<FlowcellData> getFlowcellsAll() throws IllegalArgumentException;
 	ArrayList<FlowcellData> getFlowcellsFromGeneus() throws IllegalArgumentException;
 	ArrayList<FlowcellData> getFlowcellsFromFS() throws IllegalArgumentException;
 	ArrayList<FlowcellData> getFlowcellsIncomplete() throws IllegalArgumentException;
 	ArrayList<FlowcellData> getFlowcellsComplete() throws IllegalArgumentException;
-	ArrayList<SampleData> getSampleFromGeneus() throws IllegalArgumentException;
-	ArrayList<SampleData> getSampleDataFromGeneus() throws IllegalArgumentException;
-	
+	//Get QC for the given flowcell serial
 	FlowcellData getQCforFlowcell(String serial) throws IllegalArgumentException;
-	FlowcellData getQCSampleFlowcell(String serial, String sampleID, int laneNo) throws IllegalArgumentException;
+	//Get files for flowcell
 	FlowcellData getFilesforFlowcell(String serial) throws IllegalArgumentException;
-	//SampleData getFilesforFlowcellLane(String serial) throws IllegalArgumentException;
-	FlowcellData getFilesforRunSample(String run_id, String serial, String sampleID) throws IllegalArgumentException;
-	
-	FlowcellData getCSVFiles(String run_id, String serial, String sampleID) throws IllegalArgumentException;
-	String getCSVFromDisk(String filePath) throws IllegalArgumentException;
 	//Flowcell (Merged) Analysis Reporting
 	ArrayList<FlowcellData> getAnalysisFromFS() throws IllegalArgumentException;
 	
-	//Methylation Reporting
+	/*
+	 * Sample Reporting (For tree view structure)
+	 */
+	ArrayList<SampleData> getSampleFromGeneus() throws IllegalArgumentException;
+	//Get projects from geneus
+	ArrayList<SampleData> getProjectsFromGeneus() throws IllegalArgumentException;
+	//Get QC for the given sample, flowcell and laneNo (for tree view flowcell and sample)
+	FlowcellData getQCSampleFlowcell(String serial, String sampleID, int laneNo) throws IllegalArgumentException;
+	//Get the files for given run_id, flowcell serial and sample
+	FlowcellData getFilesforRunSample(String run_id, String serial, String sampleID) throws IllegalArgumentException;
+	//Get the csv files for generating file list for plots
+	FlowcellData getCSVFiles(String run_id, String serial, String sampleID) throws IllegalArgumentException;
+	//Get the lane for given flowcell and sample
+	FlowcellData getLaneFlowcellSample(String sample_name, String flowcellSerial) throws IllegalArgumentException;
+	//Get the flowcells for a given sample_name
+	SampleData getFlowcellsforSample(String sampleProperty) throws IllegalArgumentException;
+	String getCSVFromDisk(String filePath) throws IllegalArgumentException;
+	
+	
+	/*
+	 * Methylation Reporting
+	 */
 	ArrayList<MethylationData> getMethFromGeneus() throws IllegalArgumentException;	
 	MethylationData getFilesForMeth(String serial) throws IllegalArgumentException;
 	MethylationData getQCforMeth(String serial) throws IllegalArgumentException;
@@ -44,11 +61,14 @@ public interface ECService extends RemoteService
 	//Cache management
 	String clearCache(String cachefile) throws IllegalArgumentException;
 	
+	/*
+	 * Utility Functions
+	 */
+	//Encrypt the given string 
 	String encryptURLEncoded(String srcText) throws IllegalArgumentException;
+	//Encrypt the contents passed for Guest User
 	ArrayList<String> getEncryptedData(String globalText, String laneText) throws IllegalArgumentException;
+	//Decrypt the contents passed for Guest User
 	ArrayList<String> decryptKeyword(String fcellText, String laneText)throws IllegalArgumentException;
-	FlowcellData getLaneFlowcellSample(String string, String flowcellSerial) throws IllegalArgumentException;
-	SampleData getFlowcellsforSample(String sampleProperty) throws IllegalArgumentException;
-	
 	
 }
