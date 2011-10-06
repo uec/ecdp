6bea81880f47c91ca0b4b93890cebfc4a061c506
tejassomani <tejassomani@d402b20d-0686-0410-8cf3-3d3514b17f74>
2011-10-06 22:59:47 +0000

    git-svn-id: http://www.epigenome.usc.edu/svn/ECControlCenter/branches/ECCPUIBinder@347 d402b20d-0686-0410-8cf3-3d3514b17f74

--- src/edu/usc/epigenome/eccp/client/pane/flowcellReport/FlowcellSingleItem.java
+++ src/edu/usc/epigenome/eccp/client/pane/flowcellReport/FlowcellSingleItem.java
@@ -36,6 +36,9 @@ import edu.usc.epigenome.eccp.client.pane.sampleReport.FilesDownload;
 import edu.usc.epigenome.eccp.client.pane.sampleReport.QCPlots;
 import edu.usc.epigenome.eccp.client.pane.sampleReport.QCReport;
 
+/*
+ * Class to generate a Tree structure for the Flowcells
+ */
 public class FlowcellSingleItem extends Composite  {
 
 	private static FlowcellSingleItemUiBinder uiBinder = GWT
@@ -45,6 +48,7 @@ public class FlowcellSingleItem extends Composite  {
 			UiBinder<Widget, FlowcellSingleItem> {
 	}
 	
+	//Enject resources (css styles)
 	static {
 	    UserPanelResources.INSTANCE.userPanel().ensureInjected();  
 	}
@@ -53,34 +57,44 @@ public class FlowcellSingleItem extends Composite  {
 	@UiField Tree t;
 	FlowcellData flowcell;
 	
-	
 	public FlowcellSingleItem() {
 		initWidget(uiBinder.createAndBindUi(this));
 	}
 	
+	/*
+	 * Constructor that takes an instance of FlowcellData
+	 * and create a tree structure
+	 */
 	public FlowcellSingleItem(final FlowcellData flowcellIn)
 	{
 	  flowcell=flowcellIn;
 	  initWidget(uiBinder.createAndBindUi(this));
-
+	  
+	  //Create an instance of TreeItemClick with the elements to be added at a tree level
 	  TreeItemClick fcellItem = new TreeItemClick("Fcell", flowcell.getFlowcellProperty("serial"), "technician", flowcell.getFlowcellProperty("technician"));
 	  final TreeItem fcellRoot = new TreeItem(fcellItem);
+	  //add Flowcell TreeItem to the top of the tree
 	  t.addItem(fcellRoot);
 	  fcellRoot.addItem("");
 					
+	  //Iterate over the laneNo for the flowcell
 	  for(final Integer laneNo : flowcell.lane.keySet())
 	  {
+		//Get the sampleName belonging to this laneNo
 		String samp_name = flowcell.getLaneProperty(laneNo, "name");
+		//Split the sampleNames (SampleNames are separated by "!")
 		final String sampNames[] = samp_name.split("\\!");
 		TreeItemClick laneClick = new TreeItemClick("Lane", laneNo.toString(), "(count", Integer.toString(sampNames.length) + " items)");
 		if(sampNames.length == 1)
-			laneClick = new TreeItemClick("Lane", laneNo.toString(),"(sample ", sampNames[0] +")");
-
+			laneClick = new TreeItemClick("Lane", laneNo.toString(),"(Library ", sampNames[0] +")");
+		
+		//Create TreeItem for each of the laneNos
 		final TreeItem laneItem = new TreeItem(laneClick);
 		laneItem.addItem("");
 
 		String sampleID = flowcell.getLaneProperty(laneNo, "sampleID");
 		String sampId[] = sampleID.split("\\!");
+		//Iterate over the samples and create TreeItems for each of the samples
 		for(int i=0;i<sampId.length;i++)
 		{
 		   final String sName = sampNames[i];
@@ -89,6 +103,7 @@ public class FlowcellSingleItem extends Composite  {
 		   final TreeItem sampleItem = new TreeItem(sampleClick);
 		   laneItem.addItem(sampleItem);
 		   sampleItem.addItem("");
+		   //On opening the sampleItem, a remote service call to the backend to get the runs for the given flowcell, lane and sample
 		   t.addOpenHandler(new OpenHandler<TreeItem>()
 		   {
 			 @Override
@@ -105,14 +120,17 @@ public class FlowcellSingleItem extends Composite  {
 					public void onSuccess(FlowcellData QCGot) 
 					{
 					  flowcell.laneQC = QCGot.laneQC;
+					  //Filter the results obtained by calling filterAnalysis and filterQC
 					  flowcell.filterAnalysis(flowcell.getFlowcellProperty("serial"), laneNo, sId);
 					  flowcell.filterQC(laneNo);
 					  
+					  //Iterate over each of the runs and create TreeItems for them and add to sampleItems
 					  for(String runId : flowcell.laneQC.keySet())
 					  {
 						TreeItemClick runClick = new TreeItemClick("Run", runId , "", "");
 						TreeItem runItem = new TreeItem(runClick);	
 						sampleItem.addItem(runItem);
+						//Add the QCReport, DownloadFiles and QCPlots section to the run(TreeItem)
 						runItem.addItem(new QCReport(flowcell, flowcell.getFlowcellProperty("serial"), laneNo, runId, sName, sId));
 						runItem.addItem(new FilesDownload(flowcell, flowcell.getFlowcellProperty("serial"), laneNo, runId, sName, sId));
 						runItem.addItem(new QCPlots(flowcell, flowcell.getFlowcellProperty("serial"), laneNo, runId, sName, sId));
