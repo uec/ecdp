9f9123888b3e4d63eeffc2183b419cc4bd00aa4a
tejassomani <tejassomani@d402b20d-0686-0410-8cf3-3d3514b17f74>
2011-10-10 21:28:51 +0000

    git-svn-id: http://www.epigenome.usc.edu/svn/ECControlCenter/branches/ECCPUIBinder@355 d402b20d-0686-0410-8cf3-3d3514b17f74

--- src/edu/usc/epigenome/eccp/client/pane/sampleReport/QCPlots.java
+++ src/edu/usc/epigenome/eccp/client/pane/sampleReport/QCPlots.java
@@ -15,6 +15,7 @@ import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -43,19 +44,95 @@ public class QCPlots extends Composite {
 	String flowcellSerial;
 	int laneNo;
 	String run;
-	SampleData sample;
+	FlowcellData flowcell;
 	
 	@UiField Label showPlots;
 	@UiField VerticalPanel popup;
 	@UiField FlowPanel summaryChart;
-	@UiField FlowPanel mainPanel;
-	//@UiField Label path;
 	
 	public QCPlots() {
 		initWidget(uiBinder.createAndBindUi(this));
 	}
 	
-	public QCPlots(final SampleData sampleIn, final String flowcellSerialIn, final int lane, final String runId)
+	/*
+	 * Constructor for the QCPlots, remoteService call to the backend to get the files to plot charts
+	 */
+	public QCPlots(final FlowcellData flowcellIn, final String flowcellSerialIn, final int lane, final String runId, final String library, final String sampleID)
+	{
+		flowcell = flowcellIn;
+		flowcellSerial = flowcellSerialIn;
+		laneNo = lane;
+		run = runId;
+		initWidget(uiBinder.createAndBindUi(this));
+		
+		showPlots.addClickHandler(new ClickHandler() {
+			
+			public void onClick(ClickEvent arg0) 
+			{
+				//Check for the userType and accordingly add the tab to the tabPanel
+				if(ECControlCenter.getUserType().equalsIgnoreCase("super"))
+					ECCPBinderWidget.addtoTab(popup, "Plots" + library + "_" + flowcellSerial);
+				else if(ECControlCenter.getUserType().equalsIgnoreCase("guest"))
+					GenUserBinderWidget.addtoTab(popup, "Plots" + library + "_" + flowcellSerial);
+				
+				//Get files for the given run, flowcellSerial and sample
+				remoteService.getCSVFiles(run, flowcellSerial, sampleID, new AsyncCallback<FlowcellData>() 
+				{	
+					public void onFailure(Throwable caught) 
+					{
+						summaryChart.clear();
+						summaryChart.add(new Label(caught.getMessage()));
+					}
+					public void onSuccess(FlowcellData result) 
+					{
+						summaryChart.clear();
+						summaryChart.add(new Label("Sample:" + library + " > Flowcell:" + flowcellSerial + " > Lane:"+ laneNo + " > Run:" + run));
+						flowcell.fileList = result.fileList;
+						//Filter the list of files received from the backend for the specific lane, sample and run
+						flowcell.filterFiles(lane, sampleID, run);
+						flowcell.fileList = sortBy("base");
+						
+						//Iterate over the list of files and add them to the summaryChart
+						for(int n=0; n<flowcell.fileList.size(); n++)
+						{
+							LinkedHashMap<String, String> f = flowcell.fileList.get(n);
+							String fileURI = f.containsKey("encfullpath") ? "http://webapp.epigenome.usc.edu/ECCPBinder/retrieve.jsp?resource=" + f.get("encfullpath") :  "http://www.epigenome.usc.edu/webmounts/" + f.get("dir") + "/" + f.get("base");
+							if(f.get("base").contains("ReadCount") && f.get("base").contains(".csv"))
+							{
+								summaryChart.add(new HTML("<a target=\"new\" href=\"" + fileURI + "\">" + f.get("base") + "</a>"));
+								summaryChart.add(new ChartViewer(f.get("fullpath"), ChartType.Area));
+							}
+							else if(f.get("base").contains("nmerCount") && f.get("base").contains(".csv"))
+							{
+								summaryChart.add(new HTML("<a target=\"new\" href=\"" + fileURI + "\">" + f.get("base") + "</a>"));
+								summaryChart.add(new ChartViewer(f.get("fullpath"), ChartType.Column));
+							}
+							else if(f.get("base").contains("ResultCount") && f.get("base").contains(".csv"))
+							{
+								summaryChart.add(new HTML("<a target=\"new\" href=\"" + fileURI + "\">" + f.get("base") + "</a>"));
+								summaryChart.add(new ChartViewer(f.get("fullpath"), ChartType.ResultCount));
+							}
+						}
+					}	
+			});
+		}});
+	}
+	
+	public ArrayList<LinkedHashMap<String,String>> sortBy(final String key)
+	{
+		final ArrayList<LinkedHashMap<String,String>> sortedFiles = (ArrayList<LinkedHashMap<String, String>>) flowcell.fileList.clone();
+		Collections.sort(sortedFiles, new Comparator<LinkedHashMap<String, String>>()
+		{
+			public int compare(LinkedHashMap<String, String> o1, LinkedHashMap<String, String> o2)
+			{
+				return o1.get(key).compareTo(o2.get(key));
+			}
+		});
+		return sortedFiles;
+	}
+	
+	
+ /*	public QCPlots(final SampleData sampleIn, final String flowcellSerialIn, final int lane, final String runId)
 	{
 		flowcellSerial = flowcellSerialIn;
 		sample = sampleIn;
@@ -113,20 +190,7 @@ public class QCPlots extends Composite {
 					}	
 			});
 		}});
-	}
-	
-	public ArrayList<LinkedHashMap<String,String>> sortBy(final String key)
-	{
-		final ArrayList<LinkedHashMap<String,String>> sortedFiles = (ArrayList<LinkedHashMap<String, String>>) sample.sampleFlowcells.get(flowcellSerial).fileList.clone();
-		Collections.sort(sortedFiles, new Comparator<LinkedHashMap<String, String>>()
-		{
-			public int compare(LinkedHashMap<String, String> o1, LinkedHashMap<String, String> o2)
-			{
-				return o1.get(key).compareTo(o2.get(key));
-			}
-		});
-		return sortedFiles;
-	}
+	}*/
 	
 /*	public void showChart(final String csvPath,final ChartType t, final String fileURI, final String base)
 	{
