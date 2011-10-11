d3a30b99c077e3e06a5c4396dce6741bc374a979
tejassomani <tejassomani@d402b20d-0686-0410-8cf3-3d3514b17f74>
2011-10-11 00:25:59 +0000

    git-svn-id: http://www.epigenome.usc.edu/svn/ECControlCenter/branches/ECCPUIBinder@376 d402b20d-0686-0410-8cf3-3d3514b17f74

--- src/edu/usc/epigenome/eccp/client/pane/sampleReport/SampleTreeView.java
+++ src/edu/usc/epigenome/eccp/client/pane/sampleReport/SampleTreeView.java
@@ -1,10 +1,13 @@
 package edu.usc.epigenome.eccp.client.pane.sampleReport;
 
+import java.util.ArrayList;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.logical.shared.OpenEvent;
 import com.google.gwt.event.logical.shared.OpenHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Tree;
@@ -32,17 +35,204 @@ public class SampleTreeView extends Composite
 
 	ECServiceAsync remoteService = (ECServiceAsync) GWT.create(ECService.class);
 
-	//@UiField FlowPanel vp;
 	@UiField Tree t;
-	//@UiField TreeItem sampleRoot;
-	
-	SampleData sampGeneus;
+	String projGeneus;
 	
 	public SampleTreeView() {
 		initWidget(uiBinder.createAndBindUi(this));
 	}
 	
-	public SampleTreeView(final SampleData sampleIn)
+	/*
+	 * Constructor that takes the project as an Input parameter 
+	 * and then creates the tree structure with project as the root element
+	 */
+	public SampleTreeView(String projIn, final String searchString, final boolean yesSearch)
+	{
+	   projGeneus = projIn;
+	   initWidget(uiBinder.createAndBindUi(this));
+	   TreeItemClick projCellItem = new TreeItemClick(projGeneus);
+	   final TreeItem projRoot = new TreeItem(projCellItem);
+	   t.addItem(projRoot);
+	   projRoot.addItem("");
+	   t.addOpenHandler(new OpenHandler<TreeItem>()
+			   {	
+		   @Override
+		   public void onOpen(OpenEvent<TreeItem> event) 
+		   {
+			 if(projRoot.getChildCount() > 0 && projRoot.getChildCount() <= 1)
+			 {
+			   remoteService.getSamplesForProject(projGeneus, searchString, yesSearch, new AsyncCallback<ArrayList<SampleData>>() 
+			   {
+				 public void onFailure(Throwable arg0) 
+				 {
+					 arg0.getMessage();
+				 }
+				 public void onSuccess(ArrayList<SampleData> result)
+				 {
+				   //Iterate over the List of samples and create TreeItem for each of the sample(Library)
+				    for(final SampleData samp : result)
+					{
+					  TreeItemClick sampleCellItem = new TreeItemClick("Library", samp.getSampleProperty("library"), "Date", samp.getSampleProperty("date"));
+					  final TreeItem sampleItem = new TreeItem(sampleCellItem);
+					  projRoot.addItem(sampleItem);
+
+					  //Iterate over the list of Flowcells for each of the sample and create TreeItem for each of the flowcell and add to the specific sample TreeItem(sampleItem)
+					  for(final String flowcellSerial : samp.sampleFlowcells.keySet())
+					  {
+						final TreeItemClick cellItem = new TreeItemClick("Flowcell ID", flowcellSerial, "Technician", samp.sampleFlowcells.get(flowcellSerial).flowcellProperties.get("technician"));
+						final TreeItem flowcellItem = new TreeItem(cellItem);
+						sampleItem.addItem(flowcellItem);
+						flowcellItem.addItem("");
+
+						//On opening the flowcell TreeItem give a remoteService call to the backend and get all lanes pertaining to the given sample and flowcell 
+						t.addOpenHandler(new OpenHandler<TreeItem>()
+						{
+						  @Override
+						  public void onOpen(OpenEvent<TreeItem> event1) 
+						  {
+							  TreeItem item = event1.getTarget();
+							  if(flowcellItem.getChildCount() > 0 && flowcellItem.getChildCount() <=1 && item.getText().contains("Flowcell"))
+							  {
+								remoteService.getLaneFlowcellSample(samp.getSampleProperty("library"), flowcellSerial, new AsyncCallback<FlowcellData>()
+								{
+								  public void onFailure(Throwable arg0) 
+								  {
+									arg0.getMessage();
+								  }
+								  public void onSuccess(FlowcellData fcell) 
+								  {
+									samp.sampleFlowcells.get(flowcellSerial).QClist = fcell.QClist;
+									//Iterate over the lanes and create TreeItems for each of them and add to the respective Flowcell (flowcellItem)
+									for(Integer laneNo : samp.sampleFlowcells.get(flowcellSerial).QClist.keySet())
+									{
+									  TreeItemClick laneClick = new TreeItemClick("Lane No", laneNo.toString());
+									  final TreeItem laneItem = new TreeItem(laneClick);
+									  flowcellItem.addItem(laneItem);
+
+									  ArrayList<String> s = samp.sampleFlowcells.get(flowcellSerial).QClist.get(laneNo);
+									  //Iterate over the runs belonging to the given sample, flowcell and lane and create TreeItems for each of them and add to the laneItem
+									  for(String runId : s)
+									  {
+										TreeItemClick runClick = new TreeItemClick("Run", runId);
+										TreeItem runItem = new TreeItem(runClick);	
+										laneItem.addItem(runItem);
+										runItem.addItem(new QCReport(samp.sampleFlowcells.get(flowcellSerial), flowcellSerial, laneNo, runId, samp.getSampleProperty("library"), samp.getSampleProperty("geneusID_sample")));
+										runItem.addItem(new FilesDownload(samp.sampleFlowcells.get(flowcellSerial), flowcellSerial, laneNo, runId, samp.getSampleProperty("library"), samp.getSampleProperty("geneusID_sample")));
+										runItem.addItem(new QCPlots(samp.sampleFlowcells.get(flowcellSerial), flowcellSerial, laneNo, runId, samp.getSampleProperty("library"), samp.getSampleProperty("geneusID_sample")));
+									  }
+									}
+								  }});//End remote Service call for lane and run information
+							  }
+						  }});//End Open handler for the flowcellItem 
+					  }//End for each flowcell for a library
+					}//End for each library 
+				 }});//End Remote Service call to backend
+			 }
+		   }});
+	}
+	
+	
+	
+	
+	/*
+	 * Constructor that takes the project as an Input parameter 
+	 * and then creates the tree structure with project as the root element
+	 */
+/*	public SampleTreeView(String projIn)
+	{
+	  projGeneus = projIn;
+	  initWidget(uiBinder.createAndBindUi(this));
+	  TreeItemClick projCellItem = new TreeItemClick(projGeneus);
+	  final TreeItem projRoot = new TreeItem(projCellItem);
+	  t.addItem(projRoot);
+	  projRoot.addItem("");
+	  //On open the project a remoteService call to the backend to get all the samples for the project
+	  t.addOpenHandler(new OpenHandler<TreeItem>()
+	  {	
+		@Override
+		public void onOpen(OpenEvent<TreeItem> event) 
+		{
+		  if(projRoot.getChildCount() > 0 && projRoot.getChildCount() <= 1)
+		  {
+			remoteService.getSamplesForProject(projGeneus, new AsyncCallback<ArrayList<SampleData>>() 
+			{
+			  public void onFailure(Throwable arg0) 
+			  {
+				  arg0.getMessage();
+			  }
+			  public void onSuccess(ArrayList<SampleData> result)
+			  {
+				 //Iterate over the List of samples and create TreeItem for each of the sample(Library)
+				for(final SampleData samp : result)
+				{
+				  TreeItemClick sampleCellItem = new TreeItemClick("Library", samp.getSampleProperty("library"), "Date", samp.getSampleProperty("date"));
+				  final TreeItem sampleItem = new TreeItem(sampleCellItem);
+				  projRoot.addItem(sampleItem);
+				  
+				  //Iterate over the list of Flowcells for each of the sample and create TreeItem for each of the flowcell and add to the specific sample TreeItem(sampleItem)
+				  for(final String flowcellSerial : samp.sampleFlowcells.keySet())
+				  {
+					final TreeItemClick cellItem = new TreeItemClick("Flowcell ID", flowcellSerial, "Technician", samp.sampleFlowcells.get(flowcellSerial).flowcellProperties.get("technician"));
+					final TreeItem flowcellItem = new TreeItem(cellItem);
+					sampleItem.addItem(flowcellItem);
+					flowcellItem.addItem("");
+					
+					//On opening the flowcell TreeItem give a remoteService call to the backend and get all lanes pertaining to the given sample and flowcell 
+					t.addOpenHandler(new OpenHandler<TreeItem>()
+					{
+					  @Override
+					   public void onOpen(OpenEvent<TreeItem> event1) 
+					   {
+						 TreeItem item = event1.getTarget();
+						 if(flowcellItem.getChildCount() > 0 && flowcellItem.getChildCount() <=1 && item.getText().contains("Flowcell"))
+						 {
+						   remoteService.getLaneFlowcellSample(samp.getSampleProperty("library"), flowcellSerial, new AsyncCallback<FlowcellData>()
+						   {
+							 public void onFailure(Throwable arg0) 
+							 {
+							   arg0.getMessage();
+							 }
+							 public void onSuccess(FlowcellData fcell) 
+							 {
+							   samp.sampleFlowcells.get(flowcellSerial).QClist = fcell.QClist;
+							   //Iterate over the lanes and create TreeItems for each of them and add to the respective Flowcell (flowcellItem)
+							   for(Integer laneNo : samp.sampleFlowcells.get(flowcellSerial).QClist.keySet())
+							   {
+							     TreeItemClick laneClick = new TreeItemClick("Lane No", laneNo.toString());
+							     final TreeItem laneItem = new TreeItem(laneClick);
+							     flowcellItem.addItem(laneItem);
+
+							     ArrayList<String> s = samp.sampleFlowcells.get(flowcellSerial).QClist.get(laneNo);
+							     //Iterate over the runs belonging to the given sample, flowcell and lane and create TreeItems for each of them and add to the laneItem
+							     for(String runId : s)
+							     {
+								   TreeItemClick runClick = new TreeItemClick("Run", runId);
+								   TreeItem runItem = new TreeItem(runClick);	
+								   laneItem.addItem(runItem);
+								   runItem.addItem(new QCReport(samp.sampleFlowcells.get(flowcellSerial), flowcellSerial, laneNo, runId, samp.getSampleProperty("library"), samp.getSampleProperty("geneusID_sample")));
+								   runItem.addItem(new FilesDownload(samp.sampleFlowcells.get(flowcellSerial), flowcellSerial, laneNo, runId, samp.getSampleProperty("library"), samp.getSampleProperty("geneusID_sample")));
+								   runItem.addItem(new QCPlots(samp.sampleFlowcells.get(flowcellSerial), flowcellSerial, laneNo, runId, samp.getSampleProperty("library"), samp.getSampleProperty("geneusID_sample")));
+							     }
+							   }
+							 }});//End remote Service call for lane and run information
+						 }
+					   }});//End Open handler for the flowcellItem 
+				  }//End for each flowcell for a library
+				}//End for each library 
+			  }});//End Remote Service call to backend
+		  }
+		}});
+	}*/
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	/*public SampleTreeView(final SampleData sampleIn)
 	{
 		sampGeneus = sampleIn;
 		initWidget(uiBinder.createAndBindUi(this));
@@ -151,10 +341,4 @@ public class SampleTreeView extends Composite
 		 }});//End remoteService call for getting flowcells for sample(getFlowcellsforSample) 
 		}
 	  }});//End open handler for sampleItem
-    }
-	
-	public void sampPrint()
-	{
-		
-	}
-}
+    }*/
