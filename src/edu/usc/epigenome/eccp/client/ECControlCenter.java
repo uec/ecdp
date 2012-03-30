5a4f0172f002819598daa130f19b0b03ccfd1ba7
natalia <natalia@d402b20d-0686-0410-8cf3-3d3514b17f74>
2012-03-30 20:39:54 +0000

    removed white space
    git-svn-id: http://www.epigenome.usc.edu/svn/ECControlCenter/branches/ECCPUIBinder@406 d402b20d-0686-0410-8cf3-3d3514b17f74

--- src/edu/usc/epigenome/eccp/client/ECControlCenter.java
+++ src/edu/usc/epigenome/eccp/client/ECControlCenter.java
@@ -1,15 +1,10 @@
 package edu.usc.epigenome.eccp.client;
-
-
 import java.util.Map;
-
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
-
-
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
