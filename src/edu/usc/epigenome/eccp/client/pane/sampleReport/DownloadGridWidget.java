204f594e80f6febe7fcc1ded2f98e80c0b93d574
zack <zack@d402b20d-0686-0410-8cf3-3d3514b17f74>
2012-04-24 21:57:20 +0000

    checkbox selection for multi file downloads
    git-svn-id: http://www.epigenome.usc.edu/svn/ECControlCenter/branches/gxt@419 d402b20d-0686-0410-8cf3-3d3514b17f74

--- src/edu/usc/epigenome/eccp/client/pane/sampleReport/DownloadGridWidget.java
+++ src/edu/usc/epigenome/eccp/client/pane/sampleReport/DownloadGridWidget.java
@@ -9,17 +9,22 @@ import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Label;
 
 import com.google.gwt.user.client.ui.Widget;
 import com.sencha.gxt.core.client.IdentityValueProvider;
+import com.sencha.gxt.core.client.Style.SelectionMode;
 import com.sencha.gxt.data.shared.ListStore;
 import com.sencha.gxt.data.shared.Store;
 import com.sencha.gxt.widget.core.client.ContentPanel;
+import com.sencha.gxt.widget.core.client.Dialog;
+import com.sencha.gxt.widget.core.client.Dialog.PredefinedButton;
 import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer;
 import com.sencha.gxt.widget.core.client.event.RowClickEvent;
 import com.sencha.gxt.widget.core.client.event.RowClickEvent.RowClickHandler;
 import com.sencha.gxt.widget.core.client.event.SelectEvent;
 import com.sencha.gxt.widget.core.client.form.StoreFilterField;
+import com.sencha.gxt.widget.core.client.form.TextArea;
 import com.sencha.gxt.widget.core.client.grid.CheckBoxSelectionModel;
 import com.sencha.gxt.widget.core.client.grid.ColumnConfig;
 import com.sencha.gxt.widget.core.client.grid.ColumnModel;
@@ -59,6 +64,8 @@ public class DownloadGridWidget extends Composite
 	ColumnConfig<FileData, String> cc1,cc2,cc3;
 	ListStore<FileData> store;
 	Grid<FileData> grid;
+	CheckBoxSelectionModel<FileData> sm;
+	
 	private static final FileDataModel properties = GWT.create(FileDataModel.class);
 
 	public DownloadGridWidget() {
@@ -70,7 +77,10 @@ public class DownloadGridWidget extends Composite
 	public void createFileDownloadGrid() {
 		//SET UP COLUMNS
 	     IdentityValueProvider<FileData> identity = new IdentityValueProvider<FileData>();
-	     final CheckBoxSelectionModel<FileData> sm = new CheckBoxSelectionModel<FileData>(identity);
+	     sm = new CheckBoxSelectionModel<FileData>(identity);
+	     sm.setSelectionMode(SelectionMode.SIMPLE);
+	     
+	     
 		 List<ColumnConfig<FileData, ?>> columnDefs = new ArrayList<ColumnConfig<FileData, ?>>();
 		 cc1 = new ColumnConfig<FileData, String>(properties.name(), 200, "File Name");
 		 cc2 = new ColumnConfig<FileData, String>(properties.type(), 220, "File Type");
@@ -98,8 +108,10 @@ public class DownloadGridWidget extends Composite
 				//TODO add multi download
 				int i = event.getRowIndex();
 				FileData f = store.get(i);
-				Window.open("http://webapp.epigenome.usc.edu/ECCPBinder/retrieve.jsp?resource=" + f.getDownloadLocation(), "downloading " + f.getName(), "enabled");
+				if(event.getColumnIndex() == 1)
+					Window.open("http://webapp.epigenome.usc.edu/ECCPBinder/retrieve.jsp?resource=" + f.getDownloadLocation(), "downloading " + f.getName(), "enabled");
 			}});
+		 sm.bindGrid(grid);
 	}
 	
 	public void populateGrid(ArrayList<FileData> data)
@@ -117,6 +129,31 @@ public class DownloadGridWidget extends Composite
 	public void groupByType(SelectEvent event)
 	{
 		 view.groupBy(cc2);
+	}
+	
+	@UiHandler("downloadSelected")
+	public void showSelected(SelectEvent event)
+	{
+		String fileList = "";
+		
+		 for(FileData f : sm.getSelectedItems())
+		 {
+			 fileList += "http://webapp.epigenome.usc.edu/ECCPBinder/retrieve.jsp?resource=" + f.getDownloadLocation() + "\n";
+		 }
+		 
+		 TextArea text = new TextArea();
+		 text.setText(fileList);
+		 
+		 
+		 final Dialog simple = new Dialog();
+		 simple.setHeadingText("Paste these links into your favorite download tool (Ex: wget, DownloadThemAll etc)");
+		 simple.setPredefinedButtons(PredefinedButton.YES, PredefinedButton.NO);
+		 simple.setBodyStyleName("pad-text");
+		 simple.add(text);
+		 simple.setHideOnButtonClick(true);
+		 simple.setWidth(600);
+		 simple.setHeight(400);
+		 simple.show();
 	}
 	
 	@UiHandler("organizeLocation")
