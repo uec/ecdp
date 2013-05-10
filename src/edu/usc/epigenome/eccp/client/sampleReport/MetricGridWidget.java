52a9a11ae8a4186266e0f02345d631a58a7b5ed5
zack <zack@d402b20d-0686-0410-8cf3-3d3514b17f74>
2013-05-10 21:31:05 +0000

    more readable formula
    git-svn-id: http://www.epigenome.usc.edu/svn/ECControlCenter/branches/gxt@560 d402b20d-0686-0410-8cf3-3d3514b17f74

--- src/edu/usc/epigenome/eccp/client/sampleReport/MetricGridWidget.java
+++ src/edu/usc/epigenome/eccp/client/sampleReport/MetricGridWidget.java
@@ -248,7 +248,7 @@ public class MetricGridWidget extends Composite implements HasLayout{
 				    		  formula = formula.replace("x",value.replace(",","")); 
 				    		  String eval = evalQC(formula + " ? 'Pass' : 'Fail' "); 
 				    		  if(eval.contains("Fail"))
-				    			  sb.appendHtmlConstant("<span style=\"color:red\" qtip=\"qc warning: value is not in expected range: " + formula.replace("&", " ").replace("|", " " ) + " is not true.\"><img align=\"bottom\" src=\"images/warning-small.png\"></span>");
+				    			  sb.appendHtmlConstant("<span style=\"color:red\" qtip=\"qc warning: value is not in expected range: " + formula.replace("&&", "AND").replace("||", "OR" ).replace("&", "AND").replace("|", "OR" ) + " is not true.\"><img align=\"bottom\" src=\"images/warning-small.png\"></span>");
 						}
 				    	catch(Exception e)
 						{
